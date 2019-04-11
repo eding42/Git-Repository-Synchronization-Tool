fcade317e8fa591c2ca81bf1e5edbd59ed946e29
Edward Ding <eding2019@gmail.com>
2019-04-11 05:32:14 +0000

    expand install script, must update README as well

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 
 # Copies the script to user repository directory.
 cp git-sync-tool.sh ..
+cd ..
+chmod +x git-sync-tool.sh
+echo "Installation finished! Switch to your main repository directory, and run the script with './git-sync-tool.sh'"
